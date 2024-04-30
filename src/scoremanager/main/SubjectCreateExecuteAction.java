ba9537d54ae9ecae71e1f09086cf7d504cb0041e
shuku <tky2302131@stu.o-hara.ac.jp>
2024-04-30 11:25:08 +0900

    execute

--- src/scoremanager/main/SubjectCreateExecuteAction.java
+++ src/scoremanager/main/SubjectCreateExecuteAction.java
@@ -1,5 +1,8 @@
 package scoremanager.main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -17,18 +20,20 @@ public class SubjectCreateExecuteAction extends Action {
 
 		StudentDao sDao = new StudentDao();
 
+		Map<String, String> errors = new HashMap<>();	// エラーメッセージ
+
+		String url = "";	// 画面遷移で使うurl
 		String cd = "";
 		String name = "";
 
 		cd = req.getParameter("cd");
 		name = req.getParameter("name");
 
-		cd = Integer.parseInt(name);
-		if (cd == 0) {
+		if (cd.length() != 3) {
 			errors.put("cd_error", "科目コードは3文字で入力してください");
 			req.setAttribute("errors", errors);
 		}else {
-			if (sDao.get(no) != null) {
+			if (sDao.get(cd) != null) {
 				errors.put("no_error", "科目コードが重複しています");
 				req.setAttribute("errors", errors);
 			}else {
@@ -38,12 +43,12 @@ public class SubjectCreateExecuteAction extends Action {
 
 
 				//DBへデータ保存 5
-				sDao.save(student);	// sqlを実行
+				sDao.save(subject);	// sqlを実行
 
-				url = "student_create_done.jsp";
+				url = "subject_create_done.jsp";
 				req.getRequestDispatcher(url).forward(req, res);
 			}
-			url = "StudentCreate.action";
+			url = "SubjectCreate.action";
 			req.getRequestDispatcher(url).forward(req, res);
 		}
 	}
