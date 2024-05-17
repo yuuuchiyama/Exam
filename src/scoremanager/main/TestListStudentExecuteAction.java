475ba9b6080069beb52ef95a21b47e8284012365
shuku <tky2302131@stu.o-hara.ac.jp>
2024-05-17 09:30:28 +0900

    sinnki

---
+++ src/scoremanager/main/TestListStudentExecuteAction.java
@@ -0,0 +1,83 @@
+package scoremanager.main;
+ 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Student;
+import bean.Subject;
+import bean.Teacher;
+import bean.TestListStudent;
+import dao.ClassNumDao;
+import dao.StudentDao;
+import dao.SubjectDao;
+import dao.TestListStudentDao;
+import tool.Action;
+ 
+public class TestListStudentExecuteAction extends Action {
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		HttpSession session = req.getSession();//セッション
+		Teacher teacher = (Teacher)session.getAttribute("user");
+ 
+		TestListStudentDao TestListStudentExecuteAction = new TestListStudentDao();
+		SubjectDao suDao = new SubjectDao();
+		ClassNumDao cNumDao = new ClassNumDao();
+ 
+ 
+		//ログインユーザーの学校コードをもとに、ユーザーが所属している学校の科目一覧用データを取得
+		List<Subject> list = suDao.filter(teacher.getSchool());
+ 
+		// ログインユーザーの学校コードをもとにクラス番号の一覧を取得
+		List<String> list1 = cNumDao.filter(teacher.getSchool());
+ 
+		List<TestListStudent> TestListStudentExecuteActions = null;
+ 
+ 
+		StudentDao sDao = new StudentDao();
+		Map<String, String> errors = new HashMap<>();
+		String entYearStr="";//入力された入学年度
+		String classNum ="";//入力されたクラス番号
+		String subName = "";//入力された科目
+		String studentCd = "";
+		int entYear = 0;// 入学年度
+ 
+		entYearStr = req.getParameter("f1");
+		classNum = req.getParameter("f2");
+		subName = req.getParameter("f3");
+		studentCd = req.getParameter("f4");
+ 
+		if (entYearStr !=null) {
+			//数値に変換
+			entYear = Integer.parseInt(entYearStr);
+			}
+ 
+		Student student = new Student();
+		student.setName(entYearStr);
+		student.setClassNum(classNum);
+		student.setName(subName);
+ 
+ 
+		if(entYear == 0 || classNum == null || subName == null ){
+			errors.put("f1", "入学年度とクラスと科目を選択してください");
+			req.setAttribute("errors", errors);
+		} else {
+ 
+			TestListStudentExecuteActions  = TestListStudentExecuteAction.filter(student);
+		}
+ 
+ 
+		//リクエストにデータをセット
+		//req.setAttribute("ent_year_set", );
+		//req.setAttribute("ent_year_set", entYearSet);
+		req.setAttribute("subject_list", list);
+		req.setAttribute("class_num_set", list1);
+		req.setAttribute("test_set", TestListStudentExecuteActions);
+		//JSPへフォワード
+		req.getRequestDispatcher("test_list_student.jsp").forward(req, res);
+	}
+ 
+}
